371cfe68e7a2a294eb3aaaeb9f51ec8dd7620378
BIT <BIT@DESKTOP-DOGVD1T>
2017-08-30 10:49:55 +0900

    connect

---
+++ src/main/java/com/project/controller/MainController.java
@@ -0,0 +1,17 @@
+package com.project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class MainController {
+	
+
+	@RequestMapping(value="/index")
+	public String idex() {
+		
+		return "index";
+	
+ }
+}
